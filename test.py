3e0e7bf850afb897888aec9b32d63d8a13d35b11
pro <uzzal.dna@gmail.com>
2025-12-27 16:37:55 +0600

    logger & custom exception

---
+++ test.py
@@ -0,0 +1,9 @@
+from src.noteframe.logger import logger
+from src.noteframe.custom_exception import InvalidURLException
+
+logger.info("This is a test log message from test.py")
+
+try: 
+    raise InvalidURLException()
+except Exception as e:
+    logger.error(f"Caught an exception: {e}")
