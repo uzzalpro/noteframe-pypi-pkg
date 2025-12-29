14c6cb9f96eee330e8a5dfe1f2eacd1cd1297137
pro <uzzal.dna@gmail.com>
2025-12-29 11:44:07 +0600

    add bug for readme

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/x-rst",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
